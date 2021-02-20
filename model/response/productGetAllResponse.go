61d5f3fb4c0ab5deccee736488a77ade3787c7c5
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-21 00:51:34 +0700

    refactoring get all product

--- model/response/productGetAllResponse.go
+++ model/response/productGetAllResponse.go
@@ -6,11 +6,12 @@ import (
 
 //ProductGetAllResponse ...
 type ProductGetAllResponse struct {
-	ID           uint
-	UserName     string `json:"user_name"`
-	CategoryName string `json:category_name`
-	SupplierName string `json:supplier_name`
-	ProductName  string `json:product_name`
-	Description  string `json:description`
-	CreatedAt    time.Time
+	ID              uint
+	InputBy         string `json:"input_by"`
+	CategoryName    string `json:"category_name"`
+	SupplierName    string `json:"supplier_name"`
+	ProductName     string `json:"product_name"`
+	Description     string `json:"description"`
+	ProductImageURL string `json:"product_image_url"`
+	CreatedAt       time.Time
 }
