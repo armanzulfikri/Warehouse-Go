be29daca14534f29769cece690eff116692080f9
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-21 18:39:27 +0700

    update connection

--- config/database.go
+++ config/database.go
@@ -2,32 +2,32 @@ package config
 
 import (
 	"fmt"
-	"gorm.io/gorm"
+
 	"gorm.io/driver/postgres"
-	"os"
+	"gorm.io/gorm"
 )
 
 var connDB *gorm.DB
 
 //Connect ...
 func Connect() {
-	var userDatabase, passDatabase, portDatabase, hostDatabase, nameDatabase string
+	// var userDatabase, passDatabase, portDatabase, hostDatabase, nameDatabase string
 
-	userDatabase = os.Getenv("DB_USER")
-	passDatabase = os.Getenv("DB_PASSWORD")
-	portDatabase = os.Getenv("DB_PORT")
-	hostDatabase = os.Getenv("DB_HOST")
-	nameDatabase = os.Getenv("DB_NAME")
+	// userDatabase = os.Getenv("DB_USER")
+	// passDatabase = os.Getenv("DB_PASSWORD")
+	// portDatabase = os.Getenv("DB_PORT")
+	// hostDatabase = os.Getenv("DB_HOST")
+	// nameDatabase = os.Getenv("DB_NAME")
 
-	dsn :=
-		" host=" + hostDatabase +
-			" user=" + userDatabase +
-			" password=" + passDatabase +
-			" dbname=" + nameDatabase +
-			" port=" + portDatabase +
-			" sslmode=disable TimeZone=Asia/Shanghai"
+	// dsn :=
+	// 	" host=" + hostDatabase +
+	// 		" user=" + userDatabase +
+	// 		" password=" + passDatabase +
+	// 		" dbname=" + nameDatabase +
+	// 		" port=" + portDatabase +
+	// 		" sslmode=disable TimeZone=Asia/Shanghai"
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open("postgres://iolrxtfzmvwbes:873fd93effab77843a7a71e5e8e98830e5f7222b2245aef2a314ac97f80cc516@ec2-34-198-31-223.compute-1.amazonaws.com:5432/dbhrucs3c9217j"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect to database")
 	} else {
